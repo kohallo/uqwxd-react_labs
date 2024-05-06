bc8870908fa5864b011ed31f0773f5fc033c21a3
kohallo <ohalloran.kieran@hotmail.com>
2024-05-06 15:39:13 +0100

    added functionality to increment counter app

--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import increment from '../actions'
 
+const MyButton = () => {
+  // useDispatch dispatchese event to teh store and finds out what action is to be taken and uses the right reducer to do the same
+  let dispatch = useDispatch();
+  return (
+    <button onClick={()=>dispatch(increment(1))}>Increase counter</button>
+  );
+}
+
+export default MyButton;
